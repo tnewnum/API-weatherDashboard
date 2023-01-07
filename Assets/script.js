c92d39be815f4c261d7bec7c827ae9e88b42675a
Thomas Newnum <thomas.newnum@gmail.com>
2023-01-07 18:09:06 -0500

    console.log out info from API

--- Assets/script.js
+++ Assets/script.js
@@ -1,5 +1,31 @@
-let weather = new Request ('https://api.openweathermap.org/data/2.5/forecast?lat=42.1167&lon=-86.4542&appid=16c3d00c60dcdcff5c5a0a91a5169a6a')
-fetch(weather)
-.then((response) => response.json())
-.then((data) => console.log(data));
+let searchBtn = document.querySelector('.btn')
 
+
+function search() {
+    
+    searchBtn.addEventListener('click', () => {
+        let city = document.querySelector('.city').value;
+        console.log(city);
+
+        fetch(`https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=16c3d00c60dcdcff5c5a0a91a5169a6a&units=imperial`)
+
+
+        .then(function (response) {
+        return response.json()
+        })
+        .then(function (data) {
+        console.log(data);
+        let weather = []
+         weather = data.list
+        console.log(weather)
+
+                  for (let i = 1; i < 6; i++) {
+            console.log(weather[i*8-1].weather);
+            console.log(weather[i*8-1].main.temp);
+            console.log(weather[i*8-1].wind.speed);
+            console.log(weather[i*8-1].main.humidity);
+        }
+        })   
+
+    })
+}; search()
